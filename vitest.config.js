7b0a3ec191b8f6087690042244ef8fc5903f3503
Developer <developer@example.com>
2026-02-14 12:58:45 +0800

    test: exclude e2e tests from vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     include: ['tests/**/*.{test,spec}.{js,ts}'],
-    exclude: ['node_modules', '.webpack', 'dist'],
+    exclude: ['node_modules', '.webpack', 'dist', 'tests/e2e/**'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
